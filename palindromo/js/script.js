7cc6f8daa91937b609ef91998f4de52e3c9f7353
Maccio92 <valerio362@gmail.com>
2021-11-29 16:15:51 +0100

    add function

--- palindromo/js/script.js
+++ palindromo/js/script.js
@@ -5,39 +5,39 @@ Creare una funzione per capire se la parola inserita è palindroma
 // Chiedo all'utente di inserire una parola
 let string = prompt('Inserisci una parola');
 
-// Creo un array con le lettere
-let spelling = string.split('');
-console.log(spelling);
+// // Creo un array con le lettere
+// let spelling = string.split('');
+// console.log(spelling);
 
-// Faccio il reverse delle lettere
-let spellingReverse = spelling.reverse();
+// // Faccio il reverse delle lettere
+// let spellingReverse = spelling.reverse('');
 
-// Riunisco le lettere
-let reverseString = spelling.join('');
-console.log(reverseString);
+// // Riunisco le lettere
+// let reverseString = spellingReverse.join('');
+// console.log(reverseString);
 
-// Controllo se la parola è palindroma
-if (string == reverseString){
-    console.log('La parola inserita è palindroma');
-} else {
-    console.log('La parola inserita non è palindroma');
-}
-// var carattere;
-// var parolaInversa = '';
-
-// var i = parola.length - 1;
-
-// while (i >= 0) {
-//   carattere = parola[i];
-//   parolaInversa += carattere;
-//   console.log(carattere);
-//   i--;
+// // Controllo se la parola è palindroma
+// if (string == reverseString){
+//     console.log('La parola inserita è palindroma');
+// } else {
+//     console.log('La parola inserita non è palindroma');
 // }
 
-// console.log(parolaInversa);
+// // Funzione
 
-// if(parola == parolaInversa){
-//     console.log('la parola è palindroma');
-//   } else {
-//     console.log('la parola non è palindroma');
-//   }
+function reverseStr(string) {
+    // let reverseString = "";
+    let spelling = string.split('');
+    console.log(spelling);
+    let spellingReverse = spelling.reverse('');
+    console.log(spellingReverse);
+    let reverseString = spellingReverse.join('');
+    console.log(spellingReverse);
+    return reverseString;
+}
+
+if (string == reverseStr(string)){
+    console.log('La parola inserita è palindroma');
+} else {
+    console.log('La parola inserita non è palindroma'); 
+}
